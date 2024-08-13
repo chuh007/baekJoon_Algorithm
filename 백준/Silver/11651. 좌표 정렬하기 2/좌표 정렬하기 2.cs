3c71f231caa3ef54ad8ebcfd7b8817863ce863bf
chuh007 <157554679+chuh007@users.noreply.github.com>
2024-08-14 01:49:11 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 220 ms, Memory: 22028 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+StreamReader strRead = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter strWrite = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+            StringBuilder strBild = new StringBuilder();
+            List<(int, int)> listIS = new List<(int, int)>(); // int,int를 가지는 T 리스트 만들기
+            int a = int.Parse(strRead.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                int[] s = Array.ConvertAll(strRead.ReadLine().Split(' '), int.Parse); // 정수 변환
+                listIS.Add((s[0], s[1]));
+            }
+            var sort = listIS.OrderBy(x => x.Item2).ThenBy(x => x.Item1);
+            foreach (var wow in sort)
+            {
+                strBild.AppendLine($"{wow.Item1} {wow.Item2}");
+            }
+            strWrite.Write(strBild);
+            strWrite.Flush();
