90a5b868bf8268a61d501397379db7f6d6e51a0c
chuh007 <157554679+chuh007@users.noreply.github.com>
2024-09-09 19:16:00 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+
+    int a, b;
+    cin >> a >> b;
+    cout << b - a << " " << b;
+    return 0;
+}
