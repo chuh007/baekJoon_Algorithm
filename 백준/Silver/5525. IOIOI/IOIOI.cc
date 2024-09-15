95f323bdda61b77618f7e35a5647b939a7968b83
chuh007 <157554679+chuh007@users.noreply.github.com>
2024-09-15 12:53:02 +0900

    [Silver I] Title: IOIOI, Time: 8 ms, Memory: 3680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -1,25 +1,33 @@
 #include<iostream>
 #include<string>
-#include<algorithm>
-#include<vector>
 using namespace std;
 
 
 int main()
 {
+    ios::sync_with_stdio(false); // 이 코드들은 빠른 입출력에 도움이 됨.
+    cin.tie(NULL); // C의 입출력과 C++의 입출력을 분리해서 속도를 2배 가까이 올려줌.
+    cout.tie(NULL); // 대신 이렇게 하면 C의 입출력을 사용하진 못함.
+    
     int N, M;
     string S;
-    cin >> N >> M>>S;
-    string wow = "I";
-    for (int i = 0;i < N;i++) {
-        wow += "OI";
-    }
+    cin >> N >> M >> S;
     int answer = 0;
-    while (S.find(wow)!=string::npos)
-    {
-         S.erase(S.find(wow),2);
-         answer++;
+    int count = 0;
+    for (int i = 0; i < M - 1; i++) {
+        if (S[i] == 'I' && S[i + 1] == 'O' && i + 2 < M && S[i + 2] == 'I') {
+            count++;
+            i += 1;
+            if (count == N) {
+                answer++;
+                count--;
+            }
+        }
+        else {
+            count = 0;
+        }
     }
     cout << answer;
+
     return 0;
 }
