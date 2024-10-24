e1cee75b62276114c276710b5bd1212889aef396
chuh007 <157554679+chuh007@users.noreply.github.com>
2024-10-24 22:34:33 +0900

    [Bronze V] Title: 時間 (Hour), Time: 48 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cs
@@ -0,0 +1,6 @@
+using System.IO;
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int n = int.Parse(sr.ReadLine());
+            sw.Write(n * 24);
+            sw.Flush();
