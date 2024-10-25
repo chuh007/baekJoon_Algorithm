d589cf0c55711f97d7caa6a560abfbea1f7d8e65
chuh007 <157554679+chuh007@users.noreply.github.com>
2024-10-26 05:57:29 +0900

    [Bronze V] Title: Java Warriors, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.cs
@@ -0,0 +1,6 @@
+using System.IO;
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int a = int.Parse(sr.ReadLine());
+            sw.Write(a * 4000);
+            sw.Flush();
