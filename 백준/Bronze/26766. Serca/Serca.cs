cd9858e5dceeed5b2a5e2ccf7e03216c43203350
chuh007 <157554679+chuh007@users.noreply.github.com>
2024-10-30 08:42:02 +0900

    [Bronze V] Title: Serca, Time: 132 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cs
@@ -0,0 +1,17 @@
+using System.IO;
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int a = int.Parse(sr.ReadLine());
+            for(int i = 0; i < a; i++)
+            {
+                sw.WriteLine(" @@@   @@@ \n" +
+                    "@   @ @   @\n" +
+                    "@    @    @\n" +
+                    "@         @\n" +
+                    " @       @ \n" +
+                    "  @     @  \n" +
+                    "   @   @   \n" +
+                    "    @ @    \n" +
+                    "     @     ");
+            }
+            sw.Flush();
