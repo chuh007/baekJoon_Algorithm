4e64651719aec5fb2973007300b4c9d187955bf8
chuh007 <157554679+chuh007@users.noreply.github.com>
2024-11-24 19:46:46 +0900

    [Bronze V] Title: 三方比較 (Three-Way Comparison), Time: 48 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/27328. 三方比較 （Three－Way Comparison）/三方比較 （Three－Way Comparison）.cs
@@ -0,0 +1,7 @@
+using System.IO;
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int a = int.Parse(sr.ReadLine());
+            int b = int.Parse(sr.ReadLine());
+            sw.Write(a.CompareTo(b));
+            sw.Flush();
