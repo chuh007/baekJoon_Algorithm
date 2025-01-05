17e29d0a3551fd2824f768ad4df2c18e5a0ffeb6
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-01-05 22:49:33 +0900

    [Bronze IV] Title: 방학 숙제, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.cs
@@ -0,0 +1,12 @@
+using System.IO;
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int a = int.Parse(sr.ReadLine());
+            int b = int.Parse(sr.ReadLine());
+            int c = int.Parse(sr.ReadLine());
+            int d = int.Parse(sr.ReadLine());
+            int e = int.Parse(sr.ReadLine());
+            b = (b + d - 1) / d;
+            c = (c + e - 1) / e;
+            sw.Write(b > c ? a - b : a - c);
+            sw.Flush();
