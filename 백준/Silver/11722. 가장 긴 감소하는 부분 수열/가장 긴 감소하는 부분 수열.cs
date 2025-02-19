7504cc2bb509db2533c71f2f066fe46c3707be72
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-02-19 09:41:43 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 60 ms, Memory: 6008 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cs
@@ -0,0 +1,21 @@
+using System.IO;
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int a = int.Parse(sr.ReadLine());
+            int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+            int[] arr = new int[a];
+            arr[0] = 1;
+            for (int i = 1; i < a; i++)
+            {
+                int max = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (input[j] > input[i])
+                    {
+                        max = max > arr[j] ? max : arr[j] + 1;
+                    }
+                }
+                arr[i] = max;
+            }
+            sw.Write(arr.Max());
+            sw.Flush();
