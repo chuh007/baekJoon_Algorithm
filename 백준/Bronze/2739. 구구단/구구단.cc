722a0edf52fb9e802041496ac08c1a5507404c8b
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-03-10 21:16:18 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); // 이 코드들은 빠른 입출력에 도움이 됨.
+	cin.tie(NULL); // C의 입출력과 C++의 입출력을 분리해서 속도를 2배 가까이 올려줌.
+	std::cout.tie(NULL); // 대신 이렇게 하면 C의 입출력을 사용하진 못함.
+
+	int a;
+	cin >> a;
+	for (int i = 1;i < 10;i++) {
+		cout << a << " * " << i << " = " << a * i << '\n';
+	}
+}
