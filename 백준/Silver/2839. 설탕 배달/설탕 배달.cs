1ddbababeb8e6cb181e3a2b0a7a9a57bb1be8345
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-03-15 14:09:13 +0900

    [Silver IV] Title: 설탕 배달, Time: 52 ms, Memory: 5588 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cs
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cs
@@ -1,18 +1,11 @@
-using System.Linq;
-            using System.Threading.Tasks;
-            using System.Numerics;
-            using System.Collections;
-            using System.Collections.Generic;
-            using System.Text;
             using System.IO;
             StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
             StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
-            StringBuilder sb = new StringBuilder();
 int a = int.Parse(sr.ReadLine());
             int answer = 0;
             while (true)
             {
-                if(a==1||a==2||a==4||a==7)
+                if(a==1||a==2)
                 {
                     answer = -1;
                     break;
