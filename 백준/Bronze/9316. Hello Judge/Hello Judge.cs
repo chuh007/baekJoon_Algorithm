e7a1e687610d378f74a86a297d57a6005175b118
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-04-01 19:25:27 +0900

    [Bronze IV] Title: Hello Judge, Time: 52 ms, Memory: 5908 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cs
@@ -0,0 +1,9 @@
+using System.IO;
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int a = int.Parse(sr.ReadLine());
+            for(int i = 0; i < a; i++)
+            {
+                sw.WriteLine($"Hello World, Judge {i + 1}!");
+            }
+            sw.Flush();
