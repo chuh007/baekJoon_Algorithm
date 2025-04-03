5a819c1a24116c963745052ab7415154f1b6dc3b
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-04-03 23:34:18 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false); // 이 코드들은 빠른 입출력에 도움이 됨.
+	cin.tie(NULL); // C의 입출력과 C++의 입출력을 분리해서 속도를 2배 가까이 올려줌.
+	std::cout.tie(NULL); // 대신 이렇게 하면 C의 입출력을 사용하진 못함.
+
+	int a, b, c;
+	cin >> a;
+	for (int i = 0;i < a;i++)
+	{
+		cin >> b >> c;
+		cout << "yes\n";
+	}
+}
