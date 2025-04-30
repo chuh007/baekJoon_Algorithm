4cc3a946810cf55727a100d325b36956e7999608
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-04-30 09:07:37 +0900

    [Bronze IV] Title: ПЧЕЛИЧКАТА МАЯ, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/24365. ПЧЕЛИЧКАТА МАЯ/ПЧЕЛИЧКАТА МАЯ.cs
@@ -0,0 +1,6 @@
+using System.IO;
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int[] arr = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+            sw.Write(arr[2] - arr[0]);
+            sw.Flush();
