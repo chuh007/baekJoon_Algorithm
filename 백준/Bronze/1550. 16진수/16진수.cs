57a3da23d6a06565de76d2b3b334e11c9116f363
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-05-04 21:06:16 +0900

    [Bronze II] Title: 16진수, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.cs
@@ -0,0 +1,6 @@
+using System.IO;
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+string s = sr.ReadLine();
+            sw.Write(Convert.ToInt32(s, 16));
+            sw.Flush();
