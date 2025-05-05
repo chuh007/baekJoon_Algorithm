31b95bb9a6c93a48d0e6271b00cf1dcb43676f4a
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-05-05 21:52:59 +0900

    [Gold V] Title: 토마토, Time: 72 ms, Memory: 13464 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Tomato 
+{
+public:
+    int x, y, day;
+};
+
+int box[1000][1000];
+bool isVisited[1000][1000];
+
+int main()
+{
+    ios::sync_with_stdio(false); // 이 코드들은 빠른 입출력에 도움이 됨.
+    cin.tie(NULL); // C의 입출력과 C++의 입출력을 분리해서 속도를 2배 가까이 올려줌.
+    cout.tie(NULL); // 대신 이렇게 하면 C의 입출력을 사용하진 못함.
+    int n, m;
+    int dx[4] = { 1, -1, 0, 0 };
+    int dy[4] = { 0, 0, 1, -1 };
+    cin >> n >> m;
+    queue<Tomato> q;
+    int cnt = 0;
+    int maxDay = 0;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> box[i][j];
+            if (box[i][j] == 1) {
+                q.push({ i, j, 0 });
+                isVisited[i][j] = true;
+            }
+            else if (box[i][j] == 0) cnt++;
+        }
+    }
+    while (!q.empty()) {
+		Tomato tomato = q.front();
+        q.pop();
+        maxDay = max(maxDay, tomato.day);
+
+        for (int dir = 0; dir < 4; ++dir) {
+            int nx = tomato.x + dx[dir];
+            int ny = tomato.y + dy[dir];
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+            if (isVisited[nx][ny]) continue;
+            if (box[nx][ny] != 0) continue;
+            isVisited[nx][ny] = true;
+            cnt--;
+            q.push({ nx, ny, tomato.day + 1 });
+        }
+    }
+    cout << (cnt == 0 ? maxDay : -1) << '\n';
+    return 0;
+}
