aac27e8e570a179ce0b901af8b2080f97e5cbba1
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-06-13 08:36:47 +0900

    [Bronze III] Title: Кондиционер, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/21573. Кондиционер/Кондиционер.cs
@@ -0,0 +1,10 @@
+using System.IO;
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+            string s = sr.ReadLine();
+            if (s == "freeze") sw.Write(input[0] > input[1] ? input[1] : input[0]);
+            else if (s == "heat") sw.Write(input[0] < input[1] ? input[1] : input[0]);
+            else if (s == "auto") sw.Write(input[1]);
+            else sw.Write(input[0]);
+            sw.Flush();
