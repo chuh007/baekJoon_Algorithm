36fb512d157814984c59ff4a408bb42d70fc561c
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-07-29 15:06:06 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); // 이 코드들은 빠른 입출력에 도움이 됨.
+	cin.tie(NULL); // C의 입출력과 C++의 입출력을 분리해서 속도를 2배 가까이 올려줌.
+	cout.tie(NULL); // 대신 이렇게 하면 C의 입출력을 사용하진 못함.
+
+	int n, a;
+	cin >> n;
+	int* arr = new int[n];
+	int idx1 = 0, idx2 = n - 1;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+	}
+	int answer1 = 0, answer2 = 0, minsum = INT_MAX, sub;
+	while (idx1 < idx2)
+	{
+		int sum = arr[idx1] + arr[idx2];
+		int sumabs = abs(sum);
+		if (minsum > sumabs) {
+			answer1 = arr[idx1];
+			answer2 = arr[idx2];
+			minsum = sumabs;
+		}
+		if (sum > 0) idx2--;
+		else if (sum < 0) idx1++;
+		else break;
+	}
+	cout << answer1 << ' ' << answer2;
+	return 0;
+}
