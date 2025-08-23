262ead232b2a2985401d23f9e45a3430426937e6
chuh007 <157554679+chuh007@users.noreply.github.com>
2025-08-24 01:18:45 +0900

    [Bronze IV] Title: 빵, Time: 0 ms, Memory: 2020 KB, Score: 16 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+int main()
+{
+	int n, a, b, ans = INT_MAX;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> a >> b;
+		if (a < b)
+		{
+			if (ans > b) ans = b;
+		}
+	}
+	cout << (ans == INT_MAX ? -1 : ans);
+}
